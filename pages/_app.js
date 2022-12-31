c585024c603f92d472a6795260ee1036c82cf029
soshubh <90331406+soshubh@users.noreply.github.com>
2022-12-31 22:45:48 +0530

    lets see

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
+// import { motion } from "framer-motion";
+export default function App({ Component, pageProps }) {
+  return(
+     <ThemeProvider attribute='class'>
+      <Component {...pageProps} />  
+      </ThemeProvider>
+    )
+
+}
